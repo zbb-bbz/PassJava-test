a6aebd8d8bd74cd7aeafb5eb7457584441ebc76b
张波波 <zhangbobo>
2021-02-05 11:31:35 +0800

    第一次提交

---
+++ passjava-member/src/test/java/com/jackson0714/passjava/passjavamember/PassjavaMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson0714.passjava.passjavamember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
